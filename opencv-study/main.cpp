62a5cf19988976ce6c7a2e0393b48a85eede0e9f
HHyeon <sunki1123@naver.com>
2022-03-11 19:09:55 +0900

    HoughLines() Function Test

--- opencv-study/main.cpp
+++ opencv-study/main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/video.hpp>
+#include <Windows.h>
 
 using namespace std;
 using namespace cv;
@@ -11,14 +12,35 @@ using namespace cv;
 void on_trackbar_action1(int pos, void* userdata);
 void on_trackbar_action2(int pos, void* userdata);
 
-int edge_param1 = 100;
-int edge_param2 = 200;
+void apply_houghlines(Mat* src);
+
+int edge_param1 = 500;
+int edge_param2 = 700;
 int trackbar_pos_scale = 20;
+
+#define window_name_edge "edge"
+#define window_name_src "src_clone"
+
+std::string GetCurrentDirectory()
+{
+	char buffer[360];
+	GetModuleFileNameA(NULL, buffer, 360);
+	//std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+	return std::string(buffer)/*.substr(0, pos)*/;
+}
 
 int main()
 {
+	string image_path = GetCurrentDirectory();
+	cout << image_path << endl;
+	
+	for(int i=0;i<3;i++) // back 3 times
+		image_path = image_path.substr(0, image_path.find_last_of("\\/"));
+
+	image_path += "\\ABX00051_03.front_1000x750.jpg";
+
 	Mat src;
-	src = imread("C:\\Users\\HYEON-desk\\Desktop\\ABX00051_01.iso_1000x750.jpg");
+	src = imread(image_path);
 
 	if (src.empty())
 	{
@@ -38,16 +60,66 @@ int main()
 	
 	Mat src_clone;
 	src.copyTo(src_clone);
-	putText(src_clone, "src_clone", Point(10, 30), 2, 1.0, Scalar::all(0));
-	imshow("src_clone", src_clone);
+	putText(src_clone, window_name_src, Point(10, 30), 2, 1.0, Scalar::all(0));
+	imshow(window_name_src, src_clone);
 
-	putText(edge, "edge", Point(10, 30), 2, 1.0, Scalar::all(255));
-	imshow("edge", edge);
+	putText(edge, window_name_edge, Point(10, 30), 2, 1.0, Scalar::all(255));
+	imshow(window_name_edge, edge);
 
-	waitKey();
+	while (true)
+	{
+		int ch = waitKey();
+
+		if (ch == 'q')
+			break;
+
+		switch (ch)
+		{
+		case ' ':
+			apply_houghlines(&src);
+			break;
+		}
+	}
+
 	destroyAllWindows();
 	//system("pause");
 	return 0;
+}
+
+void apply_houghlines(Mat *src)
+{
+	Mat edge;
+	Canny((*src), edge, edge_param1, edge_param2);
+
+	vector<Vec2f> lines;
+	HoughLines(edge, lines, 1, CV_PI / 180, 250);
+
+
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float r = lines[i][0];
+		float t = lines[i][1];
+		cout << "r : " << r << endl;
+		cout << "t : " << t << endl;
+		double cos_t = cos(t);
+		double sin_t = sin(t);
+		double x0 = r * cos_t;
+		double y0 = r * sin_t;
+		double alpha = 1000;
+
+		Point pt1(cvRound(x0 + alpha * (-sin_t)), cvRound(y0 + alpha * cos_t));
+		Point pt2(cvRound(x0 - alpha * (-sin_t)), cvRound(y0 - alpha * cos_t));
+		cout << "(" << pt1.x << ", " << pt1.y << "), (" << pt2.x << ", " << pt2.y << ")";
+		line(edge, pt1, pt2, Scalar::all(255), 2, LINE_AA);
+	}
+
+	//line(edge, Point(10, 10), Point(100, 100), Scalar(255, 255, 255), 2, LINE_AA);
+
+	stringstream ss;
+	ss << "lines.size() : " << lines.size();
+
+	putText(edge, ss.str(), Point(10, 30), 2, 1.0, Scalar::all(255));
+	imshow(window_name_edge, edge);
 }
 
 void apply_edge_window(Mat *src)
@@ -61,10 +133,10 @@ void apply_edge_window(Mat *src)
 	ss2 << edge_param2;
 
 	Mat edge;
-	Canny(*src, edge, (0 + edge_param1), (0 + edge_param2));
+	Canny(*src, edge, edge_param1, edge_param2);
 	putText(edge, ss1.str(), Point(10, 30), 2, 1.0, Scalar::all(255));
 	putText(edge, ss2.str(), Point(10, 60), 2, 1.0, Scalar::all(255));
-	imshow("edge", edge);
+	imshow(window_name_edge, edge);
 }
 
 void on_trackbar_action1(int pos, void* userdata)
